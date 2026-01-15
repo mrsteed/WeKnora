f5a50f682744d8081909f4d0780b323943533f0d
wizardchen <wizardchen@tencent.com>
2026-01-15 14:33:43 +0800

    fix: 修正迁移脚本中的表名拼写错误

--- migrations/mysql/000010_add_seq_id.down.sql
+++
@@ -1,5 +0,0 @@
--- Migration 000010: Remove seq_id from chunks and knowledge_tags tables
--- MySQL version
-
-ALTER TABLE chunks DROP COLUMN seq_id;
-ALTER TABLE knowledge_tags DROP COLUMN seq_id;
--- migrations/versioned/000009_add_last_faq_import_result.up.sql
+++ migrations/versioned/000009_add_last_faq_import_result.up.sql
@@ -1,4 +1,4 @@
 -- Add last_faq_import_result column to knowledge table
 -- This field stores the latest FAQ import result for FAQ type knowledge
 
-ALTER TABLE knowledge ADD COLUMN last_faq_import_result JSON DEFAULT NULL COMMENT 'Last FAQ import result (for FAQ type knowledge only)';
+ALTER TABLE knowledges ADD COLUMN last_faq_import_result JSON DEFAULT NULL;
